34e883976e3586ae786f817939f78c3278b48b4b
임동현 <79711744+ldh3907@users.noreply.github.com>
2021-11-23 15:36:19 +0900

    setting
    setting

---
+++ src/styled.d.ts
@@ -0,0 +1,16 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    red: "#E51013";
+    black: {
+      veryDark: "#141414";
+      darker: "#181818";
+      lighter: "#2F2F2F";
+    };
+    white: {
+      lighter: "#fff";
+      darker: "#e5e5e5";
+    };
+  }
+}
