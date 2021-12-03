f4031965dca1d344ecc997985f3ce54740513669
임동현 <79711744+ldh3907@users.noreply.github.com>
2021-12-03 20:56:42 +0900

    서치 구현중
    서치 구현중

--- src/Components/Search/Search.style.ts
+++ src/Components/Search/Search.style.ts
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
-export const any = styled.div``;
+export const SearchWrapper = styled.div`
+  width: 100%;
+  background-color: black;
+  display: flex;
+  margin-top: 70px;
+`;
+
+export const SearchLoader = styled.div`
+  width: 100%;
+  text-align: center;
+  color: white;
+`;
+
+export const SearchResultWrap = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: ${(props) => props.theme.black.veryDark};
+  box-sizing: border-box;
+  padding: 60px;
+  padding-top: 125px;
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 10px;
+  row-gap: 105px;
+  justify-content: center;
+  position: relative;
+  z-index: 0;
+`;
--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import { useQuery } from "react-query";
 import { useLocation } from "react-router";
 import { getSearch, IGetSearchResult } from "../../API/getSearch";
+import useMovie from "../../Hooks/Home/Movie/useMovie";
+import { makeImgPath } from "../../util/makeImgPath";
+import {
+  BoxImg,
+  boxImgVariants,
+  boxVariants,
+  Info,
+  infoVariants,
+  Movie,
+} from "../Home/Home.style";
+import { SearchLoader, SearchResultWrap, SearchWrapper } from "./Search.style";
 
 const Search: React.FC = () => {
   const location = useLocation();
@@ -10,9 +21,42 @@ const Search: React.FC = () => {
     [`movie${keyword}`, "search"],
     () => getSearch(keyword)
   );
+  const { onBoxClicked } = useMovie();
 
   console.log(data);
 
-  return <div></div>;
+  return (
+    <SearchWrapper>
+      {isLoading ? (
+        <SearchLoader>Loading...</SearchLoader>
+      ) : (
+        <SearchResultWrap>
+          {data?.results.map((result) => {
+            return (
+              <Movie
+                initial="normal"
+                whileHover="hover"
+                variants={boxVariants}
+                layoutId={result.id + ""}
+                onClick={() => onBoxClicked(result.id)}
+              >
+                <BoxImg
+                  variants={boxImgVariants}
+                  src={makeImgPath(result.backdrop_path, "w500")}
+                  alt="이미지가 없습니다."
+                  title="searchImg"
+                />
+                <Info variants={infoVariants}>
+                  <h4>{result.title}</h4>
+                  <span>평점 : ⭐{result.vote_average}</span>
+                  <span>평점 횟수 : {result.vote_count}회</span>
+                </Info>
+              </Movie>
+            );
+          })}
+        </SearchResultWrap>
+      )}
+    </SearchWrapper>
+  );
 };
 export default Search;
